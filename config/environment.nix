ebf09baaa11b060a560bee38d63949dce400ef6e
Luci <lucidashygirl@proton.me>
2025-04-22 19:57:12 -0400

    removed nvim as default editor

--- config/environment.nix
+++ config/environment.nix
@@ -1,7 +1,7 @@
 {
   environment = {
     sessionVariables = {
-      EDITOR = "/run/current-system/sw/bin/nvim";
+      # EDITOR = "/run/current-system/sw/bin/nvim";
       WLR_RENDERER = "vulkan"; # Fixes element-desktop flickering
     };
   };
